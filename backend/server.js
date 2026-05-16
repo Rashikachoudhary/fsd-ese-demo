d950684bf7c9042165b30ae0dbe88a4e0ab5bec0
Rashika <rashikachoudhary123@gmail.com>
2026-05-16 16:02:36 +0530

    fixed start script

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 const express = require("express");
 const cors = require("cors");
+
 const connectDB = require("./db");
 const routes = require("./routes");
 
@@ -19,6 +15,8 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
